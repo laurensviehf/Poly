ffb3425a828b6cfb28082b95f093ed965121e545
Laurens Viehoff <laurensrups@gmail.com>
2026-03-18 14:22:07 +0100

    check

--- Rheology/Plots/plots.py
+++ Rheology/Plots/plots.py
@@ -4,18 +4,12 @@ import pandas as pd
 
 allDataPath = r"D:\Dokumente\Uni laptop\Chemie\Poly\Rheology\Messwerte\"
 
-lampDataPath = r""
-filterDataPath = r""
 
-bandDataPath = r""
-bandTransmissionDataPath = r""
 
-allData = pd.read_csv(allDataPath, sep = ';', decimal=',', dtype=float)
-rawData = pd.read_csv(rawDataPath, sep = ';', decimal=',', dtype=float)
+Honig
+stärke
+PVP
 
-lampData = pd.read_csv(lampDataPath, sep =';', decimal = ',', dtype= float)
-filterData = pd.read_csv(filterDataPath, sep =';', decimal = ',', dtype= float)
 
-bandData = pd.read_csv(bandDataPath, sep = ';', decimal=',', dtype=float)
-bandTransmission = pd.read_csv(bandTransmissionDataPath, sep = ';', decimal=',', dtype=float)
+
 
