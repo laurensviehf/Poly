d0382b1a2c2a6bf978b4a2b6ff98efac93a7e794
Laurens Viehoff <laurensrups@gmail.com>
2026-03-21 18:13:10 +0100

    graph fertig

--- Rheology/Plots/plotsgraphen.py
+++ Rheology/Plots/plotsgraphen.py
@@ -6,12 +6,11 @@ import numpy as np
 x = np.linspace(0, 50, 4000)
 
 # 2. Funktionen definieren
-y_gerade = 4                # Lineare Funktion
-y_parabel =  4 + np.exp()           # Quadratische Funktion
-y_wurzel =  np.sqrt(x)       # Wurzelfunktion
+y_gerade = np.full_like(x,4)              # Lineare Funktion
+y_parabel =  4 + np.exp(x-4)           # Quadratische Funktion
+y_wurzel =  -np.exp(x-4) + 4       # Wurzelfunktion
 
 # 3. Plot erstellen
-plt.figure(figsize=(10, 6))
 
 plt.plot(x, y_gerade, label='newtonian', color='blue', linestyle='--')
 plt.plot(x, y_parabel, label='shear-thickenning', color='red')
@@ -20,11 +19,14 @@ plt.plot(x, y_wurzel, label='shear-thinning', color='green')
 # 4. Design-Elemente hinzufügen
 plt.xlabel(r'shear rate [$\dot\gamma_0$]')
 plt.ylabel(r'shear-stress [$\tau$]')
-plt.axhline(0, color='black', linewidth=0.5) # x-Achse
-plt.axvline(0, color='black', linewidth=0.5) # y-Achse
+#plt.axhline(0, color='black', linewidth=0.5) # x-Achse
+#plt.axvline(0, color='black', linewidth=0.5) # y-Achse
 plt.legend() # Zeigt die Beschriftungen an
 
 # Limitieren der y-Achse, damit die Parabel das Bild nicht "sprengt"
-plt.ylim(0, 50)
+plt.xticks([])
+plt.yticks([])
+plt.ylim(2, 6)
+plt.xlim(0, 5)
 
 plt.savefig(r"D:\Dokumente\Uni laptop\Chemie\Poly\Rheology\Bilder" + "\\" + "fließgraph")
