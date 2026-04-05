951af07035065b8bd175b874e5b34d9053cb18a5
Laurens Viehoff <laurensrups@gmail.com>
2026-04-05 21:58:35 +0200

    dies das annanas

---
+++ Copolymerisation/Plot.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
+
+
+init = np.array([0.00158, 0.00615, 0.01498])
+vbr = np.array([4.26735E-05, 8.40134E-05, 0.00012402])
+
+plt.figure()
+plt.plot()
